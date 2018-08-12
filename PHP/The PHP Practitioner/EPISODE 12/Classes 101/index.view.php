79013e04aa63c386cdcaddc89142e2a73b5e7cc2
haonx <qtvhao@gmail.com>
2018-08-12 23:21:11 +0700

    represent Tasks[]

--- PHP/The PHP Practitioner/EPISODE 12/Classes 101/index.view.php
+++ PHP/The PHP Practitioner/EPISODE 12/Classes 101/index.view.php
@@ -1,6 +1,27 @@
 <?php
-
 /** @var Task[] $tasks */
-foreach($tasks as $task){
-	var_export( $task);
-}
+?>
+<!doctype html>
+<html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+    </style>
+</head>
+<body>
+<header>Todo list</header>
+<div>
+    <ul>
+		<?php foreach ( $tasks as $task ): ?>
+			<?php if ( $task->isCompleted() ): ?>
+                <li style="text-decoration: line-through"><?php echo $task->getDescription(); ?></li>
+			<?php else: ?>
+                <li style="text-decoration: none;"><?php echo $task->getDescription(); ?></li>
+			<?php endif; ?>
+		<?php endforeach; ?>
+    </ul>
+</div>
+</body>
+</html>
