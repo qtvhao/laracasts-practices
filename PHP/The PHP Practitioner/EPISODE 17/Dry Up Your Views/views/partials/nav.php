e5f13b8cbfb508110a6c091ad8f374b1fd7aaa1b
haonx <qtvhao@gmail.com>
2018-08-13 21:39:05 +0700

    update EPS 17 nav.php

---
+++ PHP/The PHP Practitioner/EPISODE 17/Dry Up Your Views/views/partials/nav.php
@@ -0,0 +1,8 @@
+<ul>
+	<li>
+		<a href="index.php?path=">Home</a>
+		<a href="index.php?path=/about/">About Us</a>
+		<a href="index.php?path=/about/culture/">About Our Culture</a>
+		<a href="index.php?path=/contact/">Contact</a>
+	</li>
+</ul>
